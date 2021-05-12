7798ff7db987c97c16085092524aff1fd48e169c
TonyGaona <trgaona1@utpl.edu.ec>
2021-05-12 17:20:27 -0500

    Agrege el origen del saludo

--- src/ec/edu/utpl/poo/semana6/demo/Saludo.java
+++ src/ec/edu/utpl/poo/semana6/demo/Saludo.java
@@ -3,5 +3,6 @@ package ec.edu.utpl.poo.semana6.demo;
 public class Saludo {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("Desde Loja");
     }
 }
